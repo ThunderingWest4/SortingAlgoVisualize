e8d7900b904f3201dcde9baa69928f21a9689dc3
ThunderingWest4 <ThunderingWest4@github.com>
2020-02-11 11:18:04 -0500

    insertion sort visualizer works!!

--- insertionsort.py
+++ insertionsort.py
@@ -1,6 +1,31 @@
+import pygame
+from main import *
+import time
+
+pygame.init()
+MAX = 300
+screen = pygame.display.set_mode((MAX,MAX))
+blue = (0, 0, 255)
+
+def visualize(arr):
+    x = 0
+    y = 0
+    for a in arr:
+        pygame.draw.rect(screen, blue, (x,y,10,a*10))
+        x += 15
+    return 3
 
 def sort(s):
     for i in range(len(s)):
         curr = s[i]
-        print(yellow)
+        j = i-1
+        while(j>=0 and s[j]>curr):
+            s[j+1] = s[j]
+            j -= 1
+            print(s)
+        s[j+1] = curr
+        screen.fill((0, 0, 0))
+        visualize(s)
+        pygame.display.update()
+        time.sleep(1)
     return s
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from insertionsort import *
-import pygame
-
-pygame.init()
-
-def visualize(arr):
-    return 3
 
 toSort = [2, 4, 6, 8, 0, 1, 3, 5, 7, 9]
 
 ret = sort(toSort)
 visualize(ret)
+pygame.display.update()
+
+
 
