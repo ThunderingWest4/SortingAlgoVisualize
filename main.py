cbba1aab7edef32740156e705527c389ebef28f5
ThunderingWest4 <ThunderingWest4@github.com>
2020-02-11 11:25:07 -0500

    woo

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from insertionsort import *
 
-toSort = [2, 4, 6, 8, 0, 1, 3, 5, 7, 9]
-
+toSort = [6, 12, 4, 7, 7, 1, 3, 7, 19, 2, 4, 3, 3, 2, 15, 14, 16, 17, 8, 9, 1, 0]
+init(toSort)
 ret = sort(toSort)
-visualize(ret)
-pygame.display.update()
+import time
+for i in range(50):
+    visualize(ret)
+    pygame.display.update()
+    time.sleep(0.5)
 
 
 
